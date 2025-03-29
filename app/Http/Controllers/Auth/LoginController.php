25075213c4ee43d215608bba65f17addfd22fdd0
harold piramede <haroldpiramede@gmail.com>
2025-03-28 19:15:37 -0700

    added role table

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,11 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+    protected function aunthenticated(Request $request, $user),
+    {
+        if ($user->role == 'admin') {
+            return redirect('/admin/dashboard');
+        }else {
+            return redirect('/user/dashboard');
+        }
 }
